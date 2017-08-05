513ea170dae8d462b0fcd76732843d71cbbf11dd
DonRu <aruhrmann@atisda.de>
2017-08-05 22:57:22 +0200

    Erster Commit.

---
+++ ChecklistenGenerator_Logik/Warenwirtschaft.cs
@@ -0,0 +1,15 @@
+﻿namespace ChecklistenGenerator_Logik
+{
+    public class Warenwirtschaft
+    {
+        private static byte counter = 0;
+
+        public Warenwirtschaft()
+        {
+            counter++;
+            Name = "Keine Warenwirtschaft " + counter;
+        }
+
+        public string Name { get; set; }
+    }
+}
---
+++ ChecklistenGenerator_MSSQL/DS_Warenwirtschaft.cs
@@ -0,0 +1,19 @@
+﻿using ChecklistenGenerator_Logik;
+using System.Collections.Generic;
+
+namespace ChecklistenGenerator_MSSQL
+{
+    public class DS_Warenwirtschaft
+    {
+        public DS_Warenwirtschaft()
+        {
+            WaWis = new List<Warenwirtschaft>();
+            WaWis.Add(new Warenwirtschaft());
+            WaWis.Add(new Warenwirtschaft());
+            WaWis.Add(new Warenwirtschaft());
+            WaWis.Add(new Warenwirtschaft());
+        }
+
+        public List<Warenwirtschaft> WaWis { get; set; }
+    }
+}
